8af9a35ca6409ee2ce730e42397a545b4ac51f01
rahul1906 <rahulrmysore@gmail.com>
2017-07-22 18:45:12 +0530

    added authorization for home, login, signup page access

--- routes.py
+++ routes.py
@@ -19,6 +19,9 @@ def about():
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+  if 'email' in session:
+    return redirect(url_for('home'))
+  
   form = SignupForm()
 
   if request.method == "POST":
@@ -37,6 +40,9 @@ def signup():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+  if 'email' in session:
+    return redirect(url_for('home'))
+
   form = LoginForm()
 
   if request.method == "POST":
@@ -65,6 +71,8 @@ def logout():
 
 @app.route("/home", methods=["GET", "POST"])
 def home():
+  if 'email' not in session:
+    return redirect(url_for('login'))
   return render_template("home.html")
 
 
